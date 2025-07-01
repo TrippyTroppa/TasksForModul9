b2b73072bb41430f7ef1247376e873ff3d868040
Egor <egork2205@gmail.com>
2025-07-01 20:16:53 +0900

    Добавьте файлы проекта.

---
+++ TasksForModul9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksForModul9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exception = new Exception();
+            exception.Data.Add("Дата создания исключения :", DateTime.Now);
+        }
+
+
+    }
+}
