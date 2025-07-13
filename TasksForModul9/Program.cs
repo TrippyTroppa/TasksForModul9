e8f63b4268ccf64cc7ef5ee9db21224eda61484e
Egor <egork2205@gmail.com>
2025-07-13 19:04:48 +0900

    Задание 9.3.12

--- TasksForModul9/Program.cs
+++ TasksForModul9/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,41 +10,21 @@ namespace TasksForModul9
 {
     internal class Program
     {
-        delegate void ShowMessageDelegate();
-        delegate int SumDelegate(int a, int b, int c);
-        delegate bool CheckLengthDelegate(string row);
-
+        delegate void ShowMessageDelegate(string _message);
         static void Main(string[] args)
         {
-            Action showMessageDelegate = ShowMessage;
-            showMessageDelegate.Invoke();
+            ShowMessageDelegate showMessageDelegate = delegate (string _message)
+            {
+                Console.WriteLine(_message);
+            };
 
-            Func<int, int, int, int> sumDelegate = Sum;
-            int result = sumDelegate.Invoke(150, 30, 120);
-            Console.WriteLine(result);
-
-            Predicate<string> checkLengthDelegate = CheckLength;
-            bool status = checkLengthDelegate.Invoke("мненеодиноко");
-            Console.WriteLine(status);
-
+            showMessageDelegate.Invoke("Hello World!");
             
         }
 
-        static void ShowMessage()
-        {
-            Console.WriteLine("Я опаздываю");
-        }
+        
 
-        static int Sum(int a, int b, int c)
-        {
-            return a - b + c;
-        }
-
-        static bool CheckLength(string _row)
-        {
-            if (_row.Length > 3) return true;
-            return false;
-        }
+        
 
 
 
